1e6fd0751d506b01bb10c7298f45b7229d95a5d8
Joby Joseph <jjoseph@walmart.com>
2018-08-09 09:40:22 +0530

    JSX Preprocessing using babel

---
+++ src/like_button.js
@@ -0,0 +1,8 @@
+class LikeButton extends React.Component {
+    render() {
+        return <div>Hello World!!!</div>
+    }
+}
+
+const el = document.getElementById('app');
+ReactDOM.render(<LikeButton />, el);
